42c10e5ffea92e4e79e467efbbf0c8b36949a02f
Sunitha Iyadurai <>
2021-05-12 02:39:51 +0000

    JavaScript ExceptionsExercise 2 Completed

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
-    let result = lab.runLab(3);
-    console.log(`${lab.student} code worked: ${result === 27}`);
-  }
+    let result;
+    try {
+    result = lab.runLab(3) === 27;
+} 
+catch(TypeError) {
+  result="Error Thrown";
+        //console.log("Error thrown")
+        }
+    
+    console.log(`${lab.student} code worked: ${result}`);
+  
+}
 }
 
 let studentLabs = [
@@ -22,3 +31,25 @@ let studentLabs = [
 ];
 
 gradeLabs(studentLabs);
+let studentLabs2 = [
+   {
+      student: 'Blake',
+      myCode: function (num) {
+         return Math.pow(num, num);
+      }
+   },
+   {
+      student: 'Jessica',
+      runLab: function (num) {
+         return Math.pow(num, num);
+      }
+   },
+   {
+      student: 'Mya',
+      runLab: function (num) {
+         return num * num;
+      }
+   }
+];
+
+gradeLabs(studentLabs2);
